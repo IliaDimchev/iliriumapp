215d6a7c0f8f971698a065ae38aa9c87468e18c2
IliaDimchev <dimchev.ilia@gmail.com>
2024-12-08 16:51:23 +0000

    code structured

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Факти за България</title>
   </head>
   <body>
-    <div class="flex flex-col content-center w-screen">
+    <section class="flex flex-col content-center w-screen">
       <h1>Факти за България</h1>
       <img src="assets/images/bulgaria-map.png" />
       <table>
@@ -28,27 +28,27 @@
       <br />
       <h1>Големи градове</h1>
       <table>
-<?php
-   require_once ('config.php');
+        <?php
+          require_once ('config.php');
 
-   try {
-      $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
-      $query = $connection->query("SELECT city_name, population FROM cities ORDER BY population DESC");
-      $cities = $query->fetchAll();
+          try {
+              $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+              $query = $connection->query("SELECT city_name, population FROM cities ORDER BY population DESC");
+              $cities = $query->fetchAll();
 
-      if (empty($cities)) {
-         echo "<tr><td>Няма данни.</td></tr>\n";
-      } else {
-         foreach ($cities as $city) {
-            print "<tr><td>{$city['city_name']}</td><td align=\"right\">{$city['population']}</td></tr>\n";
-         }
-      }
-   }
-   catch (PDOException $e) {
-      print "<tr><td>Няма връзка към базата. Опитайте отново.</td></tr>\n";
-   }
-?>
+              if (empty($cities)) {
+                echo "<tr><td>Няма данни.</td></tr>\n";
+              } else {
+                foreach ($cities as $city) {
+                    print "<tr><td>{$city['city_name']}</td><td align=\"right\">{$city['population']}</td></tr>\n";
+                }
+              }
+          }
+          catch (PDOException $e) {
+              print "<tr><td>Няма връзка към базата. Опитайте отново.</td></tr>\n";
+          }
+        ?>
       </table>
-    </div>
+    </section>
   </body>
 </html>
